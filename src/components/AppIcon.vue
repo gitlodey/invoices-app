82d281d24182f62ad56cc69de243f5fc1dd99c09
a.tsyganov <a.tsyganov@smarket.hk>
2022-08-30 22:55:35 +0300

    Icon component

---
+++ src/components/AppIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <img
+    :src="src"
+    :alt="alt"
+  />
+</template>
+
+<script lang="ts" setup>
+import { defineProps, computed } from "vue";
+
+const props = defineProps(["icon", "alt"]);
+
+const src = computed(() => {
+  return `/src/assets/icons/icon-${props.icon}.svg`;
+});
+</script>
+
+<style scoped></style>
