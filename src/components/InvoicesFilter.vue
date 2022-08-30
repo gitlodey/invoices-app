54810ca46a069d8674c90c1e2f58317127241ffd
a.tsyganov <a.tsyganov@smarket.hk>
2022-08-30 22:56:29 +0300

    Invoices filter component

---
+++ src/components/InvoicesFilter.vue
@@ -0,0 +1,35 @@
+<template>
+  <select>
+    <option>Filter by status</option>
+    <option
+      v-for="period in periods"
+      :key="period.value"
+    >
+      {{ period.label }}
+    </option>
+  </select>
+</template>
+
+<script lang="ts" setup>
+import InvoicePeriods from "@/enums/InvoicePeriods";
+const periods = [
+  {
+    value: InvoicePeriods.day,
+    label: `Net ${InvoicePeriods.day} Day`,
+  },
+  {
+    value: InvoicePeriods.week,
+    label: `Net ${InvoicePeriods.week} Days`,
+  },
+  {
+    value: InvoicePeriods.twoWeeks,
+    label: `Net ${InvoicePeriods.twoWeeks} Days`,
+  },
+  {
+    value: InvoicePeriods.month,
+    label: `Net ${InvoicePeriods.month} Days`,
+  },
+];
+</script>
+
+<style scoped></style>
---
+++ src/enums/InvoicePeriods.ts
@@ -0,0 +1,8 @@
+enum InvoicePeriods {
+  day = 1,
+  week = 7,
+  twoWeeks = 14,
+  month = 30,
+}
+
+export default InvoicePeriods;
