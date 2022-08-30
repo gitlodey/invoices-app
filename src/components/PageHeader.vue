c0fb645fa8213a6cd44242456947ed041e10b32f
a.tsyganov <a.tsyganov@smarket.hk>
2022-08-30 22:57:25 +0300

    Implement page header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,3 +1,37 @@
-<template>Header</template>
+<template>
+  <div class="page-header">
+    <div class="page-header--left">
+      <h1>Invoices</h1>
+      <p>No invoice</p>
+    </div>
 
-<script lang="ts" setup></script>
+    <InvoicesFilter class="page-header--filter" />
+
+    <AppButton
+      icon="plus"
+      text="Add invoice"
+      :icon-style="{
+        backgroundColor: '#fff',
+        padding: '10px',
+        borderRadius: '20px',
+      }"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import AppButton from "@/components/AppButton.vue";
+import InvoicesFilter from "@/components/InvoicesFilter.vue";
+</script>
+<style scoped>
+.page-header {
+  display: flex;
+  align-items: center;
+}
+.page-header--left {
+  flex: 1 0 auto;
+}
+.page-header--filter {
+  margin-right: 30px;
+}
+</style>
